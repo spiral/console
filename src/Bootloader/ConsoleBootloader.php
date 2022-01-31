b1a79fd020adf81c59dc352dbac6157f267e11ee
butschster <butschster@gmail.com>
2022-01-31 10:55:20 +0300

    - Adds deprecation phpdoc for deprecated bootloaders - Starts moving bootloaders to its components

---
+++ src/Bootloader/ConsoleBootloader.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+declare(strict_types=1);
+
+namespace Spiral\Console\Bootloader;
+
+use Spiral\Boot\Bootloader\Bootloader;
+use Spiral\Boot\KernelInterface;
+use Spiral\Command\CleanCommand;
+use Spiral\Command\PublishCommand;
+use Spiral\Config\ConfiguratorInterface;
+use Spiral\Config\Patch\Append;
+use Spiral\Config\Patch\Prepend;
+use Spiral\Console\CommandLocator;
+use Spiral\Console\Console;
+use Spiral\Console\ConsoleDispatcher;
+use Spiral\Console\LocatorInterface;
+use Spiral\Console\Sequence\CallableSequence;
+use Spiral\Console\Sequence\CommandSequence;
+use Spiral\Core\Container\SingletonInterface;
+
+/**
+ * Bootloads console and provides ability to register custom bootload commands.
+ */
+final class ConsoleBootloader extends Bootloader implements SingletonInterface
+{
+    protected const DEPENDENCIES = [
+        TokenizerBootloader::class,
+    ];
+
+    protected const SINGLETONS = [
+        Console::class          => Console::class,
+        LocatorInterface::class => CommandLocator::class,
+    ];
+
+    /** @var ConfiguratorInterface */
+    private $config;
+
+    /**
+     * @param ConfiguratorInterface $config
+     */
+    public function __construct(ConfiguratorInterface $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @param KernelInterface   $kernel
+     * @param ConsoleDispatcher $console
+     */
+    public function boot(KernelInterface $kernel, ConsoleDispatcher $console): void
+    {
+        $kernel->addDispatcher($console);
+
+        $this->config->setDefaults(
+            'console',
+            [
+                'commands'  => [],
+                'configure' => [],
+                'update'    => [],
+            ]
+        );
+
+        $this->addCommand(CleanCommand::class);
+        $this->addCommand(PublishCommand::class);
+    }
+
+    /**
+     * @param class-string<\Symfony\Component\Console\Command\Command> $command
+     * @param bool $lowPriority A low priority command will be overwritten in a name conflict case.
+     *        The parameter might be removed in the next major update.
+     */
+    public function addCommand(string $command, bool $lowPriority = false): void
+    {
+        $this->config->modify(
+            'console',
+            $lowPriority
+                ? new Prepend('commands', null, $command)
+                : new Append('commands', null, $command)
+        );
+    }
+
+    /**
+     * @param array|string $sequence
+     * @param string       $header
+     * @param string       $footer
+     * @param array        $options
+     */
+    public function addConfigureSequence(
+        $sequence,
+        string $header,
+        string $footer = '',
+        array $options = []
+    ): void {
+        $this->config->modify(
+            'console',
+            $this->sequence('configure', $sequence, $header, $footer, $options)
+        );
+    }
+
+    /**
+     * @param array|string $sequence
+     * @param string       $header
+     * @param string       $footer
+     * @param array        $options
+     */
+    public function addUpdateSequence(
+        $sequence,
+        string $header,
+        string $footer = '',
+        array $options = []
+    ): void {
+        $this->config->modify(
+            'console',
+            $this->sequence('update', $sequence, $header, $footer, $options)
+        );
+    }
+
+    /**
+     * @param string $target
+     * @param mixed  $sequence
+     * @param string $header
+     * @param string $footer
+     * @param array  $options
+     * @return Append
+     */
+    private function sequence(
+        string $target,
+        $sequence,
+        string $header,
+        string $footer,
+        array $options
+    ): Append {
+        if (is_array($sequence) || $sequence instanceof \Closure) {
+            return new Append(
+                $target,
+                null,
+                new CallableSequence($sequence, $options, $header, $footer)
+            );
+        }
+
+        return new Append(
+            $target,
+            null,
+            new CommandSequence($sequence, $options, $header, $footer)
+        );
+    }
+}
