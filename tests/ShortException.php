a03c63b3a15659ca325e2d3fc13f570ddfa173df
spiralbot <ci-bot@spiralscout.com>
2026-08-05 12:00:37 +0000

    prepare release

--- tests/ShortException.php
+++ tests/ShortException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Spiral\Tests\Console;
 
-final class ShortException extends \Exception
+class ShortException extends \Exception
 {
     public function __toString(): string
     {
